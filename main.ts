882898315f58c61ed57310810caaf6c88b74485c
Narvlalejandrl <97676967+Narvlalejandrl@users.noreply.github.com>
2022-01-18 16:00:03 +0100

    todo

--- main.ts
+++ main.ts
@@ -16,3 +16,25 @@ let mySprite = sprites.create(img`
     2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
     2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
     `, SpriteKind.Player)
+mySprite.setPosition(80, 60)
+let mySprite2 = sprites.create(img`
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 7 1 1 1 1 1 7 7 7 1 1 1 1 1 7 
+    7 7 1 1 1 1 1 7 7 7 1 1 1 1 1 7 
+    7 7 f f f 1 1 7 7 7 f f f 1 1 7 
+    7 7 f f f 1 1 7 7 7 f f f 1 1 7 
+    7 7 f f f 1 1 7 7 7 f f f 1 1 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 4 4 4 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 4 4 4 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 4 4 4 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    `, SpriteKind.Enemy)
+controller.moveSprite(mySprite, 100, 100)
+mySprite2.setVelocity(50, 50)
+mySprite2.follow(mySprite, 50)
